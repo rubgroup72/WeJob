2dd769f6d7f76572e9aef56aa8366f65c0af9668
Inga629 <45121383+Inga629@users.noreply.github.com>
2019-03-02 17:52:53 +0200

    first react app commit

---
+++ React_WeJob/WeJobTest/App.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import LoggedOut from './src/screens/LoggedOut';
+import LogIn from "./src/screens/LogIn";
+
+export default class App extends Component{
+  render() {
+    return (
+      <LogIn />
+      
+      // <View style={styles.container}>
+      //   <Text style={styles.welcome}>Welcome to React Native!</Text>
+      //   <Text style={styles.instructions}>To get started, edit App.js</Text>
+      //   <Text style={styles.instructions}>{instructions}</Text>
+      // </View>
+    );
+  }
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+// });
---
+++ React_WeJob/WeJobTest/src/screens/LogIn.js
@@ -0,0 +1,106 @@
+import React, {Component} from 'react';
+import { PropTypes} from 'prop-types';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {
+    View,
+    Text,
+    ScrollView,
+    StyleSheet,
+    KeyboardAvoidingView,
+} from 'react-native';
+//import { connect } from 'react-redux';
+//import { bindActionCreators } from 'redux';
+//import { ActionCreators } from '../redux/actions';
+import colors from '../styles/colors';
+import InputField from '../components/form/InputField'
+import NextArrowButton from '../components/buttons/NextArrowButton'
+
+class LogIn extends Component {
+    handleNextButton(){
+        alert("Next button pressed");
+    }
+    render(){
+        return(
+            <KeyboardAvoidingView style={styles.wrapper}>
+                <ScrollView  behavior="padding" enabled>
+                 <View style = {styles.scrolViewWrapper}>
+                    <Text style = {styles.logInHeader}>התחבר</Text>
+                    <InputField 
+                        labelText = "דואר אלקטרוני"
+                        labelTextSize = {14}
+                        labelColor = {colors.white}
+                        textColor = {colors.white}
+                        borderBottomColor = {colors.white}
+                        inputType = "email"
+                        customStyle = {{marginBottom: 30}}
+                    >
+                    </InputField>
+                    <InputField 
+                        labelText = "סיסמא"
+                        labelTextSize = {14}
+                        labelColor = {colors.white}
+                        textColor = {colors.white}
+                        borderBottomColor = {colors.white}
+                        inputType = "password"
+                        customStyle = {{marginBottom: 30}}
+                    >
+                </InputField>
+
+                
+             </View>
+
+            </ScrollView>
+            <View style = {styles.nextButton}>
+                  <NextArrowButton
+                   handleOnPress={this.handleNextButton}
+                   />
+                </View>
+        </KeyboardAvoidingView>           
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    wrapper: {
+        display: 'flex',
+        flex: 1,
+        backgroundColor: colors.green01,
+    },
+    scrolViewWrapper:{
+        marginTop: 70,
+        flex: 1,
+        marginLeft: 20,
+        marginRight: 20,
+    },
+    ScrollView: {
+        paddingLeft: 30,
+        paddingRight: 30,
+        paddingTop: 20,
+        flex: 1,
+    },
+
+    logInHeader: {
+        fontSize: 35,
+        color: colors.white,
+        fontWeight: '300',
+        marginBottom: 40,
+    },
+    nextButton: {
+        alignItems: 'flex-start',
+        left: 20,
+        bottom: 20,
+    }
+})
+
+// const mapStateToProps = (state) => {
+//     return {
+//         loggedInStatus: state.loggedInStatus,
+//     }
+// };
+
+// const mapDispatchToProps = (dispatch) => {
+//     return bindActionCreators(ActionCreators, dispatch);
+// };
+
+
+export default LogIn;
