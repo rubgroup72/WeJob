7b52e8746c18e0c4b2e7a2a3afa68c2d772c1ea4
rubgroup72 <48026078+rubgroup72@users.noreply.github.com>
2019-03-06 12:09:10 +0200

    list distributor

--- Proj_WeJob/Proj_WeJob/Controllers/DistributorController.cs
+++ Proj_WeJob/Proj_WeJob/Controllers/DistributorController.cs
@@ -5,7 +5,7 @@ using Proj_WeJob.Models.DAL;
 
 namespace Proj_WeJob.Controllers
 {
-    public class StudentsController:ApiController
+    public class DistributorController:ApiController
     {
         // POST api/values
         //הפעלת פונקצית הוספת מפיצים במחלקת מפיץ
