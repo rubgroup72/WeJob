6212f274e896c13b8de3b7941769cccd6e20f52d
maayan <48026078+rubgroup72@users.noreply.github.com>
2019-03-31 13:58:10 +0300

    for you michel:)

---
+++ Proj_WeJob/Proj_WeJob/Models/Interst.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Proj_WeJob.Models.DAL;
+
+namespace Proj_WeJob.Models
+{
+    public class Interst
+    {
+        //properties      
+        public string Name { get; set; }
+
+        //constructor
+        public Interst(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public Interst()
+        {
+        }
+
+        // פונקציה שמחזירה רשימה של תחביבים 
+        public List<Interst> ReadInterst()
+        {
+            DBservices dbs = new DBservices();
+            return dbs.GetListInterst("DBConnectionString");
+        }
+    }
+}
