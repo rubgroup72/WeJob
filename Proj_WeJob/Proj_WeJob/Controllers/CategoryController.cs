2bf5c343d6c72a7e0166f6c472d6a4a8cddc33b4
maayan <48026078+rubgroup72@users.noreply.github.com>
2019-04-28 11:33:03 +0300

    a
    a

---
+++ Proj_WeJob/Proj_WeJob/Controllers/CategoryController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using Proj_WeJob.Models;
+
+namespace Proj_WeJob.Controllers
+{
+    public class CategoryController : ApiController
+    {
+        //הפעלת פונקציה שמחזירה את כל המפיצים
+        [HttpGet]
+        [Route("api/Category")]
+        public IEnumerable<Category> Get()
+        {
+            Category d = new Category();
+            return d.GetListCategory();
+        }
+    }
+}
---
+++ Proj_WeJob/Proj_WeJob/Models/Category.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Proj_WeJob.Models.DAL;
+
+namespace Proj_WeJob.Models
+{
+    public class Category
+    {
+        public int CategoryNo { get; set; }
+        public string CategoryName { get; set; }
+
+        public Category(int CategoryNo,string CategoryName)
+        {
+            this.CategoryName = CategoryName;
+            this.CategoryNo = CategoryNo;
+        }
+        public Category()
+        {
+        }
+        public List<Category> GetListCategory()
+        {
+            DBservices dbs = new DBservices();
+            return dbs.GetListCategory("DBConnectionString");
+        }
+    }
+}
