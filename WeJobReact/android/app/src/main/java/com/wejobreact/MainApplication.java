4b5a77654b7131d50c3f5fd07f7aa05fc82a7779
rubgrouo72 <45121383+Inga629@users.noreply.github.com>
2019-05-10 15:04:55 +0300

    add react-native-document-picker

--- WeJobReact/android/app/src/main/java/com/wejobreact/MainApplication.java
+++ WeJobReact/android/app/src/main/java/com/wejobreact/MainApplication.java
@@ -3,6 +3,7 @@ package com.wejobreact;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativedocumentpicker.ReactNativeDocumentPicker;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -34,6 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeDocumentPicker(),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage(),
             new FBSDKPackage(mCallbackManager)
